34764b23af14b0a660c295ff9fc2dfd99808e51b
zv <zv@nxvr.org>
2015-08-09 23:44:39 -0700

    Removed some org mode stuff that was cloned in org-contrib

--- contrib/!usr/zv/extensions.el
+++ contrib/!usr/zv/extensions.el
@@ -11,31 +11,6 @@
       (add-to-list 'load-path "/home/zv/.emacs.d/contrib/!usr/zv/extensions/org/contrib/lisp")
       (setq org-log-done t)
 
-      (eval-after-load 'org-indent
-        '(spacemacs|hide-lighter org-indent-mode))
-      (add-hook 'org-mode-hook 'org-indent-mode)
-
-      (evil-leader/set-key-for-mode 'org-mode
-        "mc" 'org-capture
-        "md" 'org-deadline
-        "me" 'org-export-dispatch
-        "mf" 'org-set-effort
-        "mi" 'org-clock-in
-        "mo" 'org-clock-out
-        "mm" 'org-ctrl-c-ctrl-c
-        "mq" 'org-clock-cancel
-        "mr" 'org-refile
-        "ms" 'org-schedule)
-
-      (eval-after-load "org-agenda"
-        '(progn
-           (define-key org-agenda-mode-map "j" 'org-agenda-next-line)
-           (define-key org-agenda-mode-map "k" 'org-agenda-previous-line)
-           (define-key org-agenda-mode-map
-             (kbd "SPC") evil-leader--default-map)))
-
-      (setq org-log-done t)
-
       (add-hook 'org-mode-hook 'org-indent-mode)
 
       ;; Automatically colorize source code results
@@ -59,9 +34,6 @@
     :config
     (progn
       (require 'org-indent)
-      (define-key global-map "\C-cl" 'org-store-link)
-      (define-key global-map "\C-ca" 'org-agenda)
-      (eval-after-load 'org '(zv/configure-org-evil-bindings))
       ;; Org-mime configuration
       (require 'org-mime)
       (setq org-mime-library 'mml)
@@ -71,62 +43,5 @@
       (setq org-export-with-toc nil)
       ;; Don't include section numbers with our headings
       (setq org-export-with-section-numbers nil)
-      (evil-leader/set-key-for-mode 'message-mode "mpo" 'org-mime-htmlize)
-      )))
+      (evil-leader/set-key-for-mode 'message-mode "mpo" 'org-mime-htmlize))))
 
-(defun zv/configure-org-evil-bindings ()
-  ;; Ensure we can still use M-j/M-k to move windows
-  (evil-define-key 'motion org-mode-map
-    next-buffer-key 'evil-window-next
-    prev-buffer-key 'evil-window-prev)
-
-  ;; normal state shortcuts
-  (evil-define-key 'normal org-mode-map
-    "gh" 'outline-up-heading
-    ;; to be backward compatible with older org version
-    "gj" (if (fboundp 'org-forward-same-level)
-             'org-forward-same-level
-           'org-forward-heading-same-level)
-    "gk" (if (fboundp 'org-backward-same-level)
-             'org-backward-same-level
-           'org-backward-heading-same-level)
-    "gl" 'outline-next-visible-heading
-    "t" 'org-todo
-    "T" '(lambda () (interactive) (evil-org-eol-call (lambda() (org-insert-todo-heading nil))))
-    "H" 'org-beginning-of-line
-    "L" 'org-end-of-line
-    "$" 'org-end-of-line
-    "^" 'org-beginning-of-line
-    (kbd "TAB") 'org-cycle)
-
-  ;; normal & insert state shortcuts.
-  (mapc (lambda (state)
-          (evil-define-key state org-mode-map
-            (kbd "M-l") 'org-metaright
-            (kbd "M-h") 'org-metaleft
-            (kbd "H-k") 'org-metaup
-            (kbd "H-j") 'org-metadown
-            (kbd "M-L") 'org-shiftmetaright
-            (kbd "M-H") 'org-shiftmetaleft
-            (kbd "H-K") 'org-shiftmetaup
-            (kbd "H-J") 'org-shiftmetadown
-            (kbd "M-o") '(lambda () (interactive)
-                           (evil-org-eol-call
-                            '(lambda()
-                               (org-insert-heading)
-                               (org-metaright))))
-            (kbd "M-t") '(lambda () (interactive)
-                           (evil-org-eol-call
-                            '(lambda()
-                               (org-insert-todo-heading nil)
-                               (org-metaright))))
-            ))
-        '(normal insert))
-
-  (eval-after-load 'org
-    ;; move the leader bindings to `m` prefix to be consistent with
-    ;; the rest of spacemacs bindings
-    '(evil-leader/set-key-for-mode 'org-mode
-       "a" nil "ma" 'org-agenda
-       "c" nil "mA" 'org-archive-subtree
-       "t" nil "mt" 'org-show-todo-tree)))
